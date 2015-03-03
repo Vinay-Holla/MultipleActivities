4491c2b292cc7faba904730ac719c86f7e68a0da
Vinay Keshava Holla <vinayholla@gmail.com>
2015-03-03 06:43:50 +0530

    Initial commit

---
+++ app/src/main/java/com/mycompany/multipleactivities/ViewActivity.java
@@ -0,0 +1,52 @@
+package com.mycompany.multipleactivities;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class ViewActivity extends ActionBarActivity {
+
+    TextView tvView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+
+        tvView = (TextView) findViewById(R.id.tvView);
+
+        Intent intent = getIntent();
+
+        String fName = intent.getStringExtra("fname");
+        String lName = intent.getStringExtra("lname");
+
+        tvView.setText("Your name is: " + fName + " " + lName);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
